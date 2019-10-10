7485b1316c332eb26ee0d638e992661d1854fc5c
Jonathan Norman <jnorman2123@gmail.com>
2019-10-10 19:04:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    if name == "Freddy Mercury"
+    if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
           binding.pry
         # if attribute == :favorite_ice_cream_flavors
